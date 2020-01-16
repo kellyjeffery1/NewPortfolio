e5ca6736a45ba6b338171b06c839fa50800644f5
kellyjeffery <kellyjeffery1@hotmail.com>
2020-01-16 16:29:12 +0000

    new portfolio

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.3'
 
+gem 'bootstrap', '~> 4.4.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
 # Use sqlite3 as the database for Active Record
@@ -22,6 +23,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+gem 'jquery-rails'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,91 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+var ml4 = {};
+ml4.opacityIn = [0,1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 3;
+ml4.durationIn = 800;
+ml4.durationOut = 600;
+ml4.delay = 500;
+
+// anime.timeline({loop: true})
+//   .add({
+//     targets: '.ml4 .letters-1',
+//     opacity: ml4.opacityIn,
+//     scale: ml4.scaleIn,
+//     duration: ml4.durationIn
+//   }).add({
+//     targets: '.ml4 .letters-1',
+//     opacity: 0,
+//     scale: ml4.scaleOut,
+//     duration: ml4.durationOut,
+//     easing: "easeInExpo",
+//     delay: ml4.delay
+//   }).add({
+//     targets: '.ml4 .letters-2',
+//     opacity: ml4.opacityIn,
+//     scale: ml4.scaleIn,
+//     duration: ml4.durationIn
+//   }).add({
+//     targets: '.ml4 .letters-2',
+//     opacity: 0,
+//     scale: ml4.scaleOut,
+//     duration: ml4.durationOut,
+//     easing: "easeInExpo",
+//     delay: ml4.delay
+//   }).add({
+//     targets: '.ml4 .letters-3',
+//     opacity: ml4.opacityIn,
+//     scale: ml4.scaleIn,
+//     duration: ml4.durationIn
+//   }).add({
+//     targets: '.ml4 .letters-3',
+//     opacity: 0,
+//     scale: ml4.scaleOut,
+//     duration: ml4.durationOut,
+//     easing: "easeInExpo",
+//     delay: ml4.delay
+//   }).add({
+//     targets: '.ml4',
+//     opacity: 0,
+//     duration: 500,
+//     delay: 500
+//   });
+
+$(document).ready(function(){
+
+  // Init ScrollMagic
+  var controller = new ScrollMagic.Controller();
+
+  // pin the intro
+  var pinIntroScene = new ScrollMagic.Scene({
+    triggerElement: '#intro',
+    triggerHook: 0,
+    duration: '90%'
+  })
+  .setPin('#banner')
+  .addTo(controller);
+
+  // pin again
+  var pinIntroScene2 = new ScrollMagic.Scene({
+    triggerElement: '#project01',
+    triggerHook: 0.4
+  })
+  .setPin('#banner', {pushFollowers: false})
+  .addTo(controller);
+
+  // loop through each .project element
+  $('.project').each(function(){
+
+    // build a scene
+    var ourScene = new ScrollMagic.Scene({
+      triggerElement: this.children[0],
+      triggerHook: 0.9
+    })
+    .setClassToggle(this, 'fade-in') // add class to project01
+    .addTo(controller);
+
+  });
+
+});
