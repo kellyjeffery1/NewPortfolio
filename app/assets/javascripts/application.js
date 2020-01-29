7ac0312d759c3fff4ed5aeb59703a3b250f4fb63
kellyjeffery <kellyjeffery1@hotmail.com>
2020-01-29 17:33:42 +0000

    scroll buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -97,52 +97,52 @@ $(document).ready(function(){
 //     delay: 500
 //   });
 
-$(document).ready(function() {
-$(".down").click(function() {
-     $('html, body').animate({
-         scrollTop: $(".up").offset().top
-     }, 1500);
- });
-});
+// $(document).ready(function() {
+// $(".down").click(function() {
+//      $('html, body').animate({
+//          scrollTop: $(".up").offset().top
+//      }, 1500);
+//  });
+// });
 
-$(document).ready(function() {
-$(".up").click(function() {
-     $('html, body').animate({
-         scrollTop: $(".down").offset().top
-     }, 1000);
- });
-});
-// -------------------------------------------
-$(document).ready(function() {
-$(".down2").click(function() {
-     $('html, body').animate({
-         scrollTop: $(".up2").offset().top
-     }, 1500);
- });
-});
+// $(document).ready(function() {
+// $(".up").click(function() {
+//      $('html, body').animate({
+//          scrollTop: $(".down").offset().top
+//      }, 1000);
+//  });
+// });
+// // -------------------------------------------
+// $(document).ready(function() {
+// $(".down2").click(function() {
+//      $('html, body').animate({
+//          scrollTop: $(".up2").offset().top
+//      }, 1500);
+//  });
+// });
 
-$(document).ready(function() {
-$(".up2").click(function() {
-     $('html, body').animate({
-         scrollTop: $(".down2").offset().top
-     }, 1000);
- });
-});
-//------------------------------------------------------------
-$(document).ready(function(){
-  $(window).scroll(function () {
-      if ($(this).scrollTop() > 50) {
-        $('#back-to-top').fadeIn();
-      } else {
-        $('#back-to-top').fadeOut();
-      }
-    });
-    // scroll body to 0px on click
-    $('#back-to-top').click(function () {
-      $('body,html').animate({
-        scrollTop: 0
-      }, 400);
-      return false;
-    });
-});
+// $(document).ready(function() {
+// $(".up2").click(function() {
+//      $('html, body').animate({
+//          scrollTop: $(".down2").offset().top
+//      }, 1000);
+//  });
+// });
+// //------------------------------------------------------------
+// $(document).ready(function(){
+//   $(window).scroll(function () {
+//       if ($(this).scrollTop() > 50) {
+//         $('#back-to-top').fadeIn();
+//       } else {
+//         $('#back-to-top').fadeOut();
+//       }
+//     });
+//     // scroll body to 0px on click
+//     $('#back-to-top').click(function () {
+//       $('body,html').animate({
+//         scrollTop: 0
+//       }, 400);
+//       return false;
+//     });
+// });
 
