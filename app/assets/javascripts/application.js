39bde4b67c8db8f0e81fa822ba613b4cfc1495e1
kellyjeffery <kellyjeffery1@hotmail.com>
2020-01-29 16:42:11 +0000

    scroll buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
 
   // pin the intro
   var pinIntroScene = new ScrollMagic.Scene({
-    triggerElement: '#intro',
+    triggerElement: '#banner',
     triggerHook: 0,
     duration: '95%'
   })
@@ -127,5 +127,22 @@ $(".up2").click(function() {
          scrollTop: $(".down2").offset().top
      }, 1000);
  });
+});
+//------------------------------------------------------------
+$(document).ready(function(){
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 50) {
+        $('#back-to-top').fadeIn();
+      } else {
+        $('#back-to-top').fadeOut();
+      }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 400);
+      return false;
+    });
 });
 
